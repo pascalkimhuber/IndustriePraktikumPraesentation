8cb36bd5267a374883e7ae1a0f5aa85c273b277a
Pascal Huber <pascalkimhuber@gmail.com>
2013-12-27 11:09:13 +0100

    Deleted some files.

--- README.md
+++
@@ -1,4 +0,0 @@
-IndustriePraktikumPraesentation
-===============================
-
-Präsentation zum Industrie Praktikum an der Universität Bonn im WS 13/14
--- overview/auto/overview.el
+++
@@ -1,9 +0,0 @@
-(TeX-add-style-hook "overview"
- (lambda ()
-    (LaTeX-add-labels
-     "it:SchlagerThibud"
-     "it:DalheimerGuenther"
-     "TantauWrightMiletic"
-     "it:Voss"
-     "it:Lagendijk")))
-
